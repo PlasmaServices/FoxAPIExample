fffd0419a13a136380d587a5ed6c5a2dc23c006b
herberto <herberto@herberto.xyz>
2026-01-13 21:24:44 -0800

    Added pay command, command registration changes

--- src/main/java/xyz/herberto/hytalePlugin/HytalePlugin.java
+++ src/main/java/xyz/herberto/hytalePlugin/HytalePlugin.java
@@ -5,10 +5,12 @@ import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import lombok.Getter;
 import xyz.herberto.hytalePlugin.commands.BalanceCommand;
+import xyz.herberto.hytalePlugin.commands.PayCommand;
 import xyz.herberto.hytalePlugin.commands.economy.EconomyCommands;
 import xyz.herberto.hytalePlugin.profiles.ProfileHandler;
 
 import javax.annotation.Nonnull;
+import java.util.Arrays;
 
 public final class HytalePlugin extends JavaPlugin {
 
@@ -22,14 +24,15 @@ public final class HytalePlugin extends JavaPlugin {
     @Override
     protected void setup() {
         instance = this;
-
         profileHandler = new ProfileHandler(getDataDirectory().toFile());
 
+        Arrays.asList(
+                new EconomyCommands(),
+                new BalanceCommand(),
+                new PayCommand()
+        ).forEach(command -> getCommandRegistry().registerCommand(command));
+
         getLogger().atInfo().log("Hytale Plugin has been enabled!");
-
-        getCommandRegistry().registerCommand(new EconomyCommands());
-        getCommandRegistry().registerCommand(new BalanceCommand());
-
 
     }
 
---
+++ src/main/java/xyz/herberto/hytalePlugin/commands/PayCommand.java
@@ -0,0 +1,60 @@
+package xyz.herberto.hytalePlugin.commands;
+
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.CommandSender;
+import com.hypixel.hytale.server.core.command.system.arguments.system.RequiredArg;
+import com.hypixel.hytale.server.core.command.system.arguments.types.ArgTypes;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractAsyncCommand;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.util.MessageUtil;
+import xyz.herberto.hytalePlugin.HytalePlugin;
+import xyz.herberto.hytalePlugin.profiles.ProfileHandler;
+
+import javax.annotation.Nonnull;
+import java.util.concurrent.CompletableFuture;
+
+public class PayCommand extends AbstractAsyncCommand {
+
+    private final RequiredArg<PlayerRef> playerArg;
+    private final RequiredArg<Double> amountArg;
+
+    public PayCommand() {
+        super("pay", "Pay another player money");
+        playerArg = withRequiredArg("player", "Player to pay", ArgTypes.PLAYER_REF);
+        amountArg = withRequiredArg("amount", "Amount to pay", ArgTypes.DOUBLE);
+    }
+
+    @Override
+    @Nonnull
+    protected CompletableFuture<Void> executeAsync(@Nonnull CommandContext context) {
+
+        if(!context.isPlayer()) {
+            context.sendMessage(Message.raw("You must be a player to use this command!"));
+            return CompletableFuture.completedFuture(null);
+        }
+
+        PlayerRef target = playerArg.get(context);
+        double amount = amountArg.get(context);
+
+        CommandSender player = context.sender();
+
+        if(HytalePlugin.getProfileHandler().getBalance(player.getUuid()) < amount) {
+            context.sendMessage(Message.raw("You cannot afford that!"));
+            return CompletableFuture.completedFuture(null);
+        }
+
+        HytalePlugin.getProfileHandler().removeBalance(player.getUuid(), amount);
+        HytalePlugin.getProfileHandler().addBalance(target.getUuid(), amount);
+
+
+        player.sendMessage(Message.raw("You have paid " + target.getUsername() + " $" + amount + "."));
+        target.sendMessage(Message.raw("You have received $" + amount + " from " + player.getDisplayName() + "."));
+
+
+        return CompletableFuture.completedFuture(null);
+    }
+
+
+}
